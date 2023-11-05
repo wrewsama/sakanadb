b6614464b1b15bf161c31799d3bfaa65946ffac8
wrewsama <andrewlzs512@gmail.com>
2023-11-05 12:52:12 +0800

    switch client to cmd protocol

--- sakanakv/src/client.cpp
+++ sakanakv/src/client.cpp
@@ -7,6 +7,8 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+#include <string>
+#include <vector>
 
 const size_t MAX_MSG_SIZE = 4096;
 
@@ -45,22 +47,30 @@ static int32_t write_all(int fd, char *buf, size_t size) {
     return 0;
 }
 
-static int32_t send_req(int conn_fd, const char *req) {
-    uint32_t len = (uint32_t) strlen(req);
+static int32_t send_req(int conn_fd, const std::vector<std::string> &cmd) {
+    uint32_t len = 4;
+    for (const std::string &s: cmd) {
+        len += 4 + s.size();
+    }
+
     if (len > MAX_MSG_SIZE) {
         return -1;
     }
 
-    // write
+    // write header
     char write_buf[4 + MAX_MSG_SIZE];
     memcpy(write_buf, &len, 4);
-    memcpy(&write_buf[4], req, len);
-    int32_t err = write_all(conn_fd, write_buf, len+4);
-    if (err) {
-        return err;
-    }
+    uint32_t arg_cnt = cmd.size();
+    memcpy(&write_buf[4], &arg_cnt, len);
 
-    return 0;
+    size_t idx = 8;
+    for (const std::string &s : cmd) {
+        uint32_t string_size = (uint32_t) s.size();
+        memcpy(&write_buf[idx], &string_size, 4); // size
+        memcpy(&write_buf[idx+4], s.data(), s.size()); // data
+        idx += 4 + s.size();
+    }
+    return write_all(conn_fd, write_buf, len+4);
 }
 
 static int32_t read_res(int conn_fd) {
@@ -80,12 +90,21 @@ static int32_t read_res(int conn_fd) {
     if (err) {
         return err;
     }
-    read_buf[4+len] = '\0';
-    printf("[CLIENT]: received response '%s' from server\n", &read_buf[4]);
+
+    // ensure a header is present to avoid segfaults
+    if (len < 4) {
+        printf("bad response");
+        return -1;
+    }
+
+    uint32_t rescode = 0;
+    memcpy(&rescode, &read_buf[4], 4);
+    printf("[CLIENT]: Received [%u] %.*s\n", rescode, len-4, &read_buf[8]);
+
     return 0;
 }
 
-int main() {
+int main(int argc, char **argv) {
     // open up client socket
     int client_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (client_fd < 0) {
@@ -102,22 +121,16 @@ int main() {
         die("connect()");
     }
     
-    const char *queries[3] = {"test", "testy", "tested"};
-    for (size_t i = 0; i < 3; i++) {
-        int32_t err = send_req(client_fd, queries[i]);
-        if (err) {
-            close(client_fd);
-            return 0;
-        }
+    std::vector<std::string> cmd;
+    for (int i = 1; i < argc; i++) {
+        cmd.push_back(argv[i]);
     }
-
-    for (size_t i = 0; i < 3; i++) {
-        int32_t err = read_res(client_fd);
-        if (err) {
-            close(client_fd);
-            return 0;
-        }
+    int32_t err = send_req(client_fd, cmd);
+    if (err) {
+        close(client_fd);
+        return 0;
     }
+    read_res(client_fd);
 
     close(client_fd);
     return 0;

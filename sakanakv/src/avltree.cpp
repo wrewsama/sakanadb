8ac4972845a29355c52be317c75f7503af1cb185
wrewsama <andrewlzs512@gmail.com>
2023-12-17 15:10:26 +0800

    add helper methods

--- sakanakv/src/avltree.cpp
+++ sakanakv/src/avltree.cpp
@@ -8,3 +8,23 @@ struct AvlNode {
    AvlNode *right = NULL;
    AvlNode *parent = NULL;
 };
+
+static void avl_init(AvlNode *node) {
+    node->depth = 1;
+    node->cnt = 1;
+    node->left = NULL;
+    node->right = NULL;
+    node->parent = NULL;
+}
+
+static uint32_t avl_depth(AvlNode *node) {
+    return node ? node->depth: 0;
+}
+
+static uint32_t avl_cnt(AvlNode *node) {
+    return node ? node->cnt : 0;
+}
+
+static uint32_t max(uint32_t lhs, uint32_t rhs) {
+    return lhs < rhs ? rhs : lhs;
+}

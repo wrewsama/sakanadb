9687b61960d8e18f4363e7f49be17b2d5d3bb222
wrewsama <andrewlzs512@gmail.com>
2024-05-17 21:40:30 +0800

    set up server

---
+++ sakanakv2/server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func handleCli(conn net.Conn) error {
+    buf := make([]byte, 1024)
+    readLen, err := conn.Read(buf)
+    if err != nil {
+        return fmt.Errorf("error reading from conn: err=%w", err)
+    }
+
+    fmt.Printf("Received: %s", string(buf[:readLen]))
+
+    reply := fmt.Sprintf("Echo: %s", string(buf[:readLen]))
+    if _, err := conn.Write([]byte(reply)); err != nil {
+        return fmt.Errorf("error writing to conn: err=%w", err)
+    }
+    return nil
+}
+
+func main() {
+    // TODO: make these env vars
+    HOST := "localhost"
+    PORT := 3535
+
+    server, err := net.Listen("tcp", fmt.Sprintf("%s:%d", HOST, PORT))
+    if err != nil {
+        fmt.Printf("Error listening: err=%v", err)
+    }
+    defer server.Close()
+
+    fmt.Printf("Listening on host %s with port %d", HOST, PORT)
+    for {
+        conn, err := server.Accept()
+        if err != nil {
+            fmt.Printf("Error accepting client connection: err=%+v", err)
+        }
+
+        handleCli(conn) 
+    }
+}

096284f8e404ba0812777bfd74169ae57ce83950
wrewsama <andrewlzs512@gmail.com>
2024-06-09 20:41:36 +0800

    set up pipeline for sakanakv2

--- sakanakv2/server/go.mod
+++ sakanakv2/server/go.mod
@@ -3,7 +3,6 @@ module github.com/wrewsama/sakanadb/sakanakv2/server
 go 1.22.3
 
 require (
-	github.com/bytedance/mockey v1.2.10
 	github.com/ovechkin-dm/mockio v0.6.2
 	github.com/smartystreets/goconvey v1.8.1
 )
@@ -14,5 +13,4 @@ require (
 	github.com/ovechkin-dm/go-dyno v0.1.3 // indirect
 	github.com/petermattis/goid v0.0.0-20230904192822-1876fd5063bc // indirect
 	github.com/smarty/assertions v1.15.0 // indirect
-	golang.org/x/arch v0.0.0-20201008161808-52c3e6f60cff // indirect
 )
--- sakanakv2/server/tcp/service_test.go
+++ sakanakv2/server/tcp/service_test.go
@@ -36,3 +36,29 @@ func Test_ReadNBytes(t *testing.T) {
 		})
 	})	
 }
+
+func Test_WriteBytes(t *testing.T) {
+	Convey("Test ReadNBytes", t, func() {
+		service := NewTCPService()
+		Convey("conn read err", func() {
+			mockConn := Mock[net.Conn]()
+			WhenDouble(
+				mockConn.Write(Any[[]byte]())).ThenReturn(0, errors.New(""))
+
+			err := service.WriteBytes(mockConn, []byte("xdd"))
+
+			Verify(mockConn, Once()).Write(Any[[]byte]())
+			So(err, ShouldNotBeNil)
+		})
+		Convey("conn read ok", func() {
+			mockConn := Mock[net.Conn]()
+			WhenDouble(
+				mockConn.Write(Any[[]byte]())).ThenReturn(1, nil)
+
+			err := service.WriteBytes(mockConn, []byte("xdd"))
+
+			Verify(mockConn, Times(3)).Write(Any[[]byte]())
+			So(err, ShouldBeNil)
+		})
+	})	
+}

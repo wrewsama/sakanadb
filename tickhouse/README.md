f9a886a6a601617605c5cb1ae37f176d5b1ff63a
wrewsama <andrewlzs512@gmail.com>
2026-04-26 20:29:22 +0800

    add raw data

--- tickhouse/README.md
+++ tickhouse/README.md
@@ -1,3 +1,11 @@
 # tickhouse
 
 A columnar, time-series database for highly-compressed storage of L1 Market Data Ticks.
+
+# Rationale
+This project serves to experiment with the compression ratios of various storage solutions, as well as compression methods.
+
+# Comparisons
+| setup | size (bytes) |
+| :--- | :--- |
+| naive JSON | 8003616 |
---
+++ tickhouse/tickhouse-repl/scripts/prep_data.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import json
+
+def main():
+    raw_data_dir = Path("../raw_data").resolve()
+    files = raw_data_dir.glob("*.json")
+    res: list[dict[str, str]] = []
+    for file in files:
+        data = json.loads(file.read_text())
+        symbol = data["Meta Data"]["2. Symbol"]
+        timeseries = data["Time Series (Daily)"]
+        for time, bar in timeseries.items():
+            flattened_bar = {
+                "time": time,
+                "symbol": symbol,
+                "open": bar["1. open"],
+                "high": bar["2. high"],
+                "low": bar["3. low"],
+                "close": bar["4. close"],
+                "volume": bar["5. volume"],
+            }
+            res.append(flattened_bar) 
+    with open(raw_data_dir / "raw_data.json", "w") as f:
+        json.dump(res, f, indent=4)
+
+if __name__ == "__main__":
+    main()
